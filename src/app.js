7e0a5185a5a8789df48f9d3aa97eac277c999a2d
Chris Banno <cbanno@my.bcit.ca>
2026-02-17 10:29:14 -0800

    Add: Demo07 COMP1800 according to lecture time Feb 17, 2026

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import '/src/styles/style.css';
 // Custom global JS code (shared with all pages)can go here.
 //--------------------------------------------------------------
 
+
+
 // This is an example function. Replace it with your own logic.
 function sayHello() {
   // TODO: implement your logic here
