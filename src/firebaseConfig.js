b54bd9bcd81506251cc4224149081a5a791523ab
Chris Banno <cbanno@my.bcit.ca>
2026-03-03 23:07:05 -0800

    Add firebase database

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,9 +8,11 @@
 // environment variables (.env file in project root).
 // ---------------------------------------------------------
 
+
 // Import Firebase SDK modules (using Firebase v9 modular syntax)
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // ---------------------------------------------------------
 // Read Firebase configuration from Vite environment variables.
@@ -42,3 +44,4 @@ const app = initializeApp(firebaseConfig);
 // or signout operations (that's why we export it).
 // ---------------------------------------------------------
 export const auth = getAuth(app);
+export const db = getFirestore(app);
